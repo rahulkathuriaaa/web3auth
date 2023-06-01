fca7845a6b0664fda97f0a1f5b241fe07a4fc106
rahulkathuriaaa <rahulkathuria008@gmail.com>
2023-06-01 19:32:43 +0530

    auth done

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MyButton from './Button';
-
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 export default function App() {
   return (
     <View style={styles.container}>
--- Button.jsx
+++ Button.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {Button, View} from 'react-native';
-import * as WebBrowser from "@toruslabs/react-native-web-browser";
-// or  import * as WebBrowser from "expo-web-browser"; // (for expo)
-import {Web3Auth} from "@web3auth/react-native-sdk";
+// import * as WebBrowser from "@toruslabs/react-native-web-browser";
+  import * as WebBrowser from "expo-web-browser"; // (for expo)
+import Web3Auth, { LOGIN_PROVIDER, OPENLOGIN_NETWORK } from "@web3auth/react-native-sdk";
+// import * as WebBrowser from "expo-web-browser";
 
+// const resolvedRedirectUrl =
+//   Constants.appOwnership == AppOwnership.Expo || Constants.appOwnership == AppOwnership.Guest
+//     ? Linking.createURL("web3auth", {})
+//     : Linking.createURL("web3auth", { scheme: scheme });
+
+    // const scheme = "exp://192.168.29.244:19000"; // Or your desired app redirection scheme
+// const resolvedRedirectUrl = `${scheme}://openlogin`;
 
 const web3auth = new Web3Auth(WebBrowser, {
   clientId: "BDa3KzVEtDEf8NxJZrIC4YndpEbAoAsop3DkcyqQN-kevgGJe-k-jGywdUhfQzY5qhTDR8kVRmoQm68Ot66QjCQ",
-  network: Network.TESTNET,
+  network: OPENLOGIN_NETWORK.TESTNET,
   whiteLabel: {
     name: "Web3Auth ReactNative Sample App",
     logoLight: "https://images.web3auth.io/web3auth-logo-w.svg",
@@ -19,10 +27,19 @@ const web3auth = new Web3Auth(WebBrowser, {
     },
   },
 });
-const state = await web3auth.login({
-    loginProvider: LoginProvider.GOOGLE,
-    redirectUrl: resolvedRedirectUrl,
-  });
+async function state() {
+    console.log("yes")
+    await  web3auth.login({
+        loginProvider: LOGIN_PROVIDER.GOOGLE,
+        redirectUrl: `exp://192.168.29.244:19000`,
+        
+  })
+}
+
+// const state = await web3auth.login({
+//     loginProvider: LOGIN_PROVIDER.GOOGLE,
+//     redirectUrl: resolvedRedirectUrl,
+//   });
 const MyButton = () => {
   return (
     <View>
